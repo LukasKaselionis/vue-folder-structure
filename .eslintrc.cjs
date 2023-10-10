5a929763f12529b28f3220b8dedf2d42d9c63f61
Lukas Kašelionis <lukas@scoding.com>
2023-10-10 12:59:55 +0300

    Connected prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,14 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  env: { node: true },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:prettier/recommended"
   ],
+  plugins: ["prettier"],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: "latest"
   }
 }
