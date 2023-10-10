c02f75fcc3aeb9e9be7ddf60d568be3ad0d45fd5
Lukas Kašelionis <lukas@scoding.com>
2023-10-10 13:02:23 +0300

    Connected prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.mount('#app')
+app.use(createPinia());
+app.mount("#app");
