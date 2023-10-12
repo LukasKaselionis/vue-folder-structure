7fdf6aaf58dd45af1accf14c953f70abe6d89d12
Lukas Kašelionis <lukas@scoding.com>
2023-10-12 11:04:08 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { join } from "path";
 
-export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
+const resolve = (dir: string): string => join(__dirname, dir);
+
+const config: UserConfig = defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": resolve("src")
+        },
+    },
 });
+
+export default config;
